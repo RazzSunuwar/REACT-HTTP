94c5c86be4b866319923cdc2802ddedd273f0ef7
RazzSunuwar <razzkoinch5@gmail.com>
2020-04-07 23:14:46 +0545

    HTTP post Request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import PostList from './components/PostList';
+import PostForm from './components/PostForm';
 
 function App() {
   return (
     <div className="App">
-      <PostList />
+      {/* <PostList /> */}
+      <PostForm />
     </div>
   );
 }
